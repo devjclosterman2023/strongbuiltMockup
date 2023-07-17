568bf7b171fa765b62e4d3cb031a3f93c29118da
Jason C <Jason@web-mo.com>
2023-07-17 11:24:00 -0700

    second commit

--- themes/customtheme/footer.php
+++ themes/customtheme/footer.php
@@ -12,28 +12,35 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-		   <a href="/">
-			<div class="logowords">
-			<img src="/wp-content/uploads/2023/07/cropped-StrongBuilt_USA_Logo_Electric-2048x711-1.png" alt="Picture of Strongbuilt Logo" class="headerLogo"> 
-		   </a>
-		   <h4>The Most Comfortable <br> Call You'll Ever Make</h4>
-		   <div class="footernav">	 
-		   <a href="#"></a>Blog
+		<div class="max">
+		<div class="top-section">
+			<div class="section"><img src="/wp-content/uploads/2023/07/cropped-StrongBuilt_USA_Logo_Electric-2048x711-1.png" alt="Picture of Strongbuilt Logo" class="headerLogo"> 
+            </div>
+			<div class="section"><h4>The Most Comfortable <br> Call You'll Ever Make</h4></div>
+			<div class="section">
+			 <a href="#"></a>Blog
 		     <a href="#"></a>About
 		     <a href="#"></a>Contact
-		     <a href="#"></a>Service Areas
-			</div>	
+		     <a href="#"></a>Service Areas 
+			</div>
 		</div>
-		<div class="tucson footernav">
-	      <h3>Tucson</h3><hr>
-	      <p>4759 S Butterfield Dr</p> 
-		  <p>(520)-257-6268</p>
-        </div>
-	   <div class="albuquerque footernav">
-		<h3>Albuquerque</h3><hr>
-       </div>
-		</div><!-- .site-info -->
+		<div class="bottom-section">
+			<div class="section"> <h3>Tucson</h3>
+			<h6>4759 S Butterfield Dr<span>(520)-263-8275</span></h6> 
+			<h6>Tucson, AZ 85714<span>24 x 7 * 365</span></h6> 
+			<h3>Arizona License</h3>
+			<h5>AZROC: 749856. 836478, 92746, <br> 72645, 12536</h5>
+		    </div>
+
+			<div class="section"><h3>Albuquerque</h3>
+			<h6>3540 Pan America Freeway NE <span>(505)-257-6268</span></h6>
+            <h6>Suite E Albuquerque, NM 87107 <span>24 x 7 * 365</span></h6>
+			<h3>New Mexico License</h3>
+			<h5>401693</h5>
+		    </div>
+			<div class="section">section 6</div>
+		    </div>
+		</div> <!-- max -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
